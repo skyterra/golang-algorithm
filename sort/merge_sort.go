770f0769063eb0443444a0ec42d5a55c3cc40e86
Qinghui Yao <sky_terra@126.com>
2025-05-15 11:11:52 +0800

    feat(merge-sort): add merge sort with sentinel value optimization in merge2 function

--- sort/merge_sort.go
+++ sort/merge_sort.go
@@ -1,5 +1,7 @@
 package sort
 
+import "math"
+
 // MergeSort sorts the slice in ascending order using the merge sort algorithm.
 func MergeSort(a []int, p, q int) {
 	if p >= q {
@@ -11,6 +13,19 @@ func MergeSort(a []int, p, q int) {
 	MergeSort(a, m+1, q)
 
 	merge(a, p, m, q)
+}
+
+// MergeSort2 sorts the slice in ascending order using the merge sort algorithm.
+func MergeSort2(a []int, p, q int) {
+	if p >= q {
+		return
+	}
+
+	m := p + (q-p)/2
+	MergeSort2(a, p, m)
+	MergeSort2(a, m+1, q)
+
+	merge2(a, p, m, q)
 }
 
 // merge combines two sorted subarrays a[p:m+1] and a[m+1:q+1] into a single sorted subarray a[p:q+1].
@@ -42,6 +57,29 @@ func merge(a []int, p, m, q int) {
 		}
 
 		if r < len(right) {
+			a[i] = right[r]
+			r++
+		}
+	}
+}
+
+// merge2 combines two sorted subarrays a[p:m+1] and a[m+1:q+1] into a single sorted subarray a[p:q+1].
+func merge2(a []int, p, m, q int) {
+	l, r := 0, 0
+
+	left := make([]int, m-p+1+1)
+	copy(left, a[p:m+1])
+	left[len(left)-1] = math.MaxInt
+
+	right := make([]int, q-m+1)
+	copy(right, a[m+1:q+1])
+	right[len(right)-1] = math.MaxInt
+
+	for i := p; i < q+1; i++ {
+		if left[l] <= right[r] {
+			a[i] = left[l]
+			l++
+		} else {
 			a[i] = right[r]
 			r++
 		}
--- sort/merge_sort_test.go
+++ sort/merge_sort_test.go
@@ -13,3 +13,17 @@ func TestMergeSort(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkMergeSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := GenSlice(10000, 1000000)
+		MergeSort(s, 0, len(s)-1)
+	}
+}
+
+func BenchmarkMergeSort2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := GenSlice(10000, 1000000)
+		MergeSort2(s, 0, len(s)-1)
+	}
+}
